d39b5730d3154d4322f16f87d05bf591f7441119
Itssagarcode <sagarmaithil70@gmail.com>
2023-12-22 23:27:54 +0530

    first commit

---
+++ frontend/src/Pages/Product.jsx
@@ -0,0 +1,25 @@
+
+import React, { useContext } from 'react';
+import Breadcrum from '../Components/Breadcrum/Breadcrum';
+import { ShopContext } from '../Context/ShopContext';
+import { useParams } from 'react-router-dom';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
+import DescriptioBox from '../Components/DescriptionBox/DescriptioBox';
+import RelatedProducts from '../Components/RelatedProducts/RelatedProducts';
+
+const Product = () => {
+
+const {all_product} = useContext(ShopContext);
+const {productId} = useParams();
+const product = all_product.find((e)=> e.id === Number(productId))
+  return (
+    <div>
+      <Breadcrum product={product}/>
+      <ProductDisplay product={product}/>
+      <DescriptioBox/>
+      <RelatedProducts/>
+    </div>
+  )
+}
+
+export default Product
